f4673921056c9b1f2d4062807cf37275c04f13ca
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-08-09 15:05:28 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -5,7 +5,7 @@ couple_1 = first == second
 couple_2 = second == third
 couple_3 = first == third
 if first == second == third:
-    print(first, second, third)
+    print('3')
 elif couple_1 or couple_2 or couple_3:
     print('2')
 else:
