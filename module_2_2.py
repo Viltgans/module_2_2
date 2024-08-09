23d07432a6b1636356a76b8060c3e51a0687909e
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-08-09 14:42:53 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,12 @@
+first = int(input())
+second = int(input())
+third = int(input())
+couple_1 = first == second
+couple_2 = second == third
+couple_3 = first == third
+if first == second == third:
+    print(first, second, third)
+elif couple_1 or couple_2 or couple_3:
+    print('2')
+else:
+    print('0')
